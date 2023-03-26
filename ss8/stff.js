c728865fc39326ddf6017b227b0ca98c11d4f070
skill-issue-smh <sigmagorath@gmail.com>
2023-03-26 10:51:31 +0700

    ok

---
+++ ss8/stff.js
@@ -0,0 +1,9 @@
+
+var a = document.getElementById('nut1')
+console.log(a)
+function hidetext(){
+    var element1 = document.getElementById('jk');
+    var element2 = document.getElementById('troll');
+    element1.classList.add('begone')
+    element2.classList.remove('begone')
+}
